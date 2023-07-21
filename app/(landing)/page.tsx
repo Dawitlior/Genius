e037800364b3a6e5290ae418a04aba1cc22676f7
Dawitlior <dawitlior777@gmail.com>
2023-07-21 13:19:10 +0300

    sign in sign up btn's

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 const LandingPage = () => {
     return <div>
         landing page (Unprotected)
+        <div>
+            <Link href="/sign-in">
+                <Button>
+                    Login
+                </Button>
+            </Link>
+
+            <Link href="/sign-up">
+                <Button>
+                    Register
+                </Button>
+            </Link>
+
+        </div>
     </div>
 }
 
