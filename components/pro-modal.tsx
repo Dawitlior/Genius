5816e17c6c5952c1c0d34d9bd1278c45996342dc
Dawitlior <dawitlior777@gmail.com>
2023-07-23 00:39:55 +0300

    toast

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -16,6 +16,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 
 const tools = [
@@ -66,7 +67,7 @@ export const ProModal = () => {
       window.location.href = response.data.url;
     } catch (error) {
       console.log(error,"STRIPE_CLIENT_ERROR")
-      // toast.error("Something went wrong");
+      toast.error("Something went wrong");
     } finally {
       setLoading(false);
     }
--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { Zap } from "lucide-react";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface SubscriptionButtonProps {
   isPro: boolean;
@@ -20,7 +21,7 @@ export const SubscriptionButton = ({
       const response = await axios.get("/api/stripe");
       window.location.href = response.data.url;
     } catch (error) {
-      console.log("Something went wrong");
+      toast.error("Something went wrong.")
     } finally {
       setLoading(false);
     }
