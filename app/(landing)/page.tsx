97e71912607a776506111c4b2d0b10bb841ab542
Dawitlior <dawitlior777@gmail.com>
2023-07-23 11:26:29 +0300

    landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,17 @@
+import { LandingNavbar } from "@/components/Landing-navbar"
+import { LandingContent } from "@/components/landing-content"
+import { LandingHero } from "@/components/landing-hero"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
 const LandingPage = () => {
-    return <div>
-        landing page (Unprotected)
-        <div>
-            <Link href="/sign-in">
-                <Button>
-                    Login
-                </Button>
-            </Link>
-
-            <Link href="/sign-up">
-                <Button>
-                    Register
-                </Button>
-            </Link>
-
-        </div>
-    </div>
+    return (
+     <div className="h-full">
+        <LandingNavbar />
+        <LandingHero />
+        <LandingContent />
+     </div>  
+    )
 }
 
 export default LandingPage
