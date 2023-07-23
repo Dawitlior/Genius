a91f7b98eb1188d2e204f75c40d83975703e6780
Dawitlior <dawitlior777@gmail.com>
2023-07-23 23:44:19 +0300

    add

--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import axios from "axios";
-import { Zap } from "lucide-react";
 import { useState } from "react";
-import toast from "react-hot-toast";
+import { Zap } from "lucide-react";
+import { toast } from "react-hot-toast";
 
-interface SubscriptionButtonProps {
-  isPro: boolean;
-}
+import { Button } from "@/components/ui/button";
 
 export const SubscriptionButton = ({
-  isPro = false,
-}: SubscriptionButtonProps) => {
+  isPro = false
+}: {
+  isPro: boolean;
+}) => {
   const [loading, setLoading] = useState(false);
 
   const onClick = async () => {
     try {
       setLoading(true);
+
       const response = await axios.get("/api/stripe");
+
       window.location.href = response.data.url;
     } catch (error) {
-      toast.error("Something went wrong.")
+      toast.error("Something went wrong");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <Button variant={isPro ? "default" : "premium"} onClick={onClick}>
+    <Button variant={isPro ? "default" : "premium"} disabled={loading} onClick={onClick} >
       {isPro ? "Manage Subscription" : "Upgrade"}
       {!isPro && <Zap className="w-4 h-4 ml-2 fill-white" />}
     </Button>
-  );
+  )
 };
